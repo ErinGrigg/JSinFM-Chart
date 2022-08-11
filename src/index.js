f563aba1826448f8e0bf09d1bdc2255ac5135fa2
Erin Tripp <erin@pkinformation.com>
2022-08-11 14:24:15 -0400

    Data comes from FM

--- src/index.js
+++ src/index.js
@@ -1,59 +1,42 @@
 import c3 from 'c3';
 
-const chart = c3.generate({
-  bindto: '#chart',
-  size: {
-    width: 600,
-    height: 400
-  },
-  color: {
-    pattern: ['#565d77', '#79C8B1']
-  },
-  data: {
+window.loadChart = function (json) {
 
-    colors: {
-      data1: '#565d77',
-      data2: '#79C8B1'
+  const obj = JSON.parse(json);
+  const data = obj.data;
+  
+  const chart = c3.generate({
+    bindto: '#chart',
+    size: {
+      width: 600,
+      height: 400
     },
-    type: "bar",
+    color: {
+      pattern: ['#565d77', '#79C8B1']
+    },
     axis: {
       x: {
         type: 'category'
       }
     },
-    json: [
-      {
-        "month": "Jan",
-        "Peaches": 327,
-        "Bananas": 437,
-        "Pears": 111
+    data: {
+      colors: {
+        data1: '#565d77',
+        data2: '#79C8B1'
       },
-      {
-        "month": "Feb",
-        "Peaches": 555,
-        "Bananas": 454,
-        "Pears": 222
+      type: "bar",
+  
+      json: data,
+      keys: {
+        x: 'month',
+        value: ['Peaches', 'Bananas', 'Pears']
       },
-      {
-        "month": "Mar",
-        "Peaches": 242,
-        "Bananas": 864,
-        "Pears": 343
-      },
-      {
-        "month": "Apr",
-        "Peaches": 222,
-        "Bananas": 454,
-        "Pears": 434
-      }
-    ],
-    keys: {
-      x: 'month',
-      value: ['Peaches', 'Bananas', 'Pears']
     },
+  
+  
   },
+  );
+};
 
 
-},
 
-);

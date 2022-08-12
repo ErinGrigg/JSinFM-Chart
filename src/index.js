eeac919702e30a754f1e9ce81d723755456bc134
Erin Tripp <erin@pkinformation.com>
2022-08-12 17:28:58 -0400

    Chart info comes from FM

--- src/index.js
+++ src/index.js
@@ -4,36 +4,48 @@ window.loadChart = function (json) {
 
   const obj = JSON.parse(json);
   const data = obj.data;
+  const type = obj.type;
+  const colorBanana = obj.colors.Bananas;
+  const colorPeach = obj.colors.Peaches;
+  const colorPear = obj.colors.Pears;
+  const showLabels = obj.showLables;
+
+  console.log(type);
+  console.log(colorBanana);
+  console.log(colorPeach);
+  console.log(colorPear);
+  console.log(showLabels);
   
   const chart = c3.generate({
     bindto: '#chart',
-    size: {
-      width: 600,
-      height: 400
-    },
-    color: {
-      pattern: ['#565d77', '#79C8B1']
-    },
+
     axis: {
       x: {
-        type: 'category'
+        type: 'category',
+        label: 'Months',
+        position: 'outer-center'
       }
+    },
+    legend: {
+      item: {
+        onclick: function () {},
+      },
     },
     data: {
       colors: {
-        data1: '#565d77',
-        data2: '#79C8B1'
+        Peaches: colorBanana,
+        Bananas: colorPeach,
+        Pears: colorPear,
       },
-      type: "bar",
-  
+      type: type,
+      onclick: function () {},
       json: data,
       keys: {
         x: 'month',
         value: ['Peaches', 'Bananas', 'Pears']
       },
     },
-  
-  
+    
   },
   );
 };
